f8535cb9613274132fcd2e4114ec219bfc35e726
heesunc <heesun9113@naver.com>
2023-03-06 23:44:38 +0900

    Pause_1
    일시정지 이미지 추가해서 누르면 시간 멈추도록

---
+++ Assets/Scripts/Pause/Pause.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public Button pauseBtn;
+    public GameObject TimeUI;
+    void Update()
+    {
+        Debug.Log("Update");
+        transform.Translate(Time.deltaTime, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate");
+    }
+
+    public void OnTogglePauseButton()
+    {
+        if (Time.timeScale == 0) //멈춰있으면
+        {
+            Time.timeScale = 1f; //시작
+        }
+        else //움직이면
+        {
+            Time.timeScale = 0; //멈추기
+            TimeUI.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Pause/PauseBtn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseBtn : MonoBehaviour
+{
+    public GameObject TimeUI;
+
+    public void ExitBtn()
+    {
+        // Pause를 누르고 "나가기" 버튼을 누르면 시작화면으로 이동
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void ProgressBtn()
+    {
+        // Pause를 누르고 "계속 진행" 버튼을 누르면 3초 후 게임 계속할 수 있도록
+        TimeUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
