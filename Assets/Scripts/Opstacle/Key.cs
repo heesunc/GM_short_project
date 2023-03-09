857780d8a4be4285619fc3b0ef572a332d0819d1
heesunc <heesun9113@naver.com>
2023-03-09 19:26:01 +0900

    Scene Merge

--- Assets/Scripts/Opstacle/Key.cs
+++ Assets/Scripts/Opstacle/Key.cs
@@ -10,7 +10,7 @@ public class Key : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        manager = GameObject.Find("GameManager").GetComponent<GameManager>();    
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
